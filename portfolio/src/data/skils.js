a5109334dee7f9b91873d47b21507b33e6772364
Gu Hyeonmo <90318301+Yeonhul@users.noreply.github.com>
2021-10-22 16:01:19 +0900

    Delete portfolio directory

--- portfolio/src/data/skils.js
+++
@@ -1,9 +0,0 @@
-export default [
-    { name : 'HTML'},
-    { name : 'CSS'},
-    { name : 'JAVASCRIPT'},
-    { name : 'ECMAScript6'},
-    { name : 'JQUERY'},
-    { name : 'VUE'},
-    { name : 'SASS, SCSS'}
-]
